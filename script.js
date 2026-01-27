497b1c859a9807c8f71d7ef052c3bc6e979370df
DeathScythe1021 <jenny90yeh@gmail.com>
2026-01-27 17:47:58 +0800

    feat: add image wave animation

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 // homepage-lang-btn
 const langButton = document.querySelectorAll(".lang-btn");
 
@@ -20,45 +18,45 @@ const tl1 = gsap.timeline({
     trigger: ".story-section",
     start: "top top",
     end: "+=2000",
-   toggleActions: "play none none none",
-   scrub:2,
-   pin:true
+    toggleActions: "play none none none",
+    scrub: 2,
+    pin: true,
   },
-  delay: 0.8
+  delay: 0.8,
 });
 
 // 跑馬燈
 const marquee = gsap.to(".title-marquee", {
-  xPercent: 100,   
-  duration: 20,     
-  repeat: -1,      
-  ease: "none",   
-  paused: true     
+  xPercent: 100,
+  duration: 20,
+  repeat: -1,
+  ease: "none",
+  paused: true,
 });
 
 // 文字介紹效果
-const introduce = gsap.from(".introduce",{
-    xPercent:400,
-    stagger: 0.15,
-    duration:0.5,
-    ease: "power3.out",
-    delay:0.5,
-    paused:true
-  });
+const introduce = gsap.from(".introduce", {
+  xPercent: 400,
+  stagger: 0.15,
+  duration: 0.5,
+  ease: "power3.out",
+  delay: 0.5,
+  paused: true,
+});
 
 tl1
-// 標題左右分開
+  // 標題左右分開
   .to(".title-text", {
     xPercent: (index) => {
       return index === 0 ? -200 : 200;
     },
     opacity: 1,
     duration: 0.8,
-    delay:0.2,
+    delay: 0.2,
     ease: "power2.out",
   })
 
-//   照片浮現
+  //   照片浮現
   .from(
     ".kon-img",
     {
@@ -67,70 +65,134 @@ tl1
       duration: 0.5,
       ease: "power3.out",
     },
-    "-=0.5"
+    "-=0.5",
   )
 
   // 跑馬燈開跑
-  .to(".title-marquee", {
-    opacity: 1,
-    duration: 0.1,
-    
-    onStart: () => {
-      marquee.play(); 
+  .to(
+    ".title-marquee",
+    {
+      opacity: 1,
+      duration: 0.1,
+
+      onStart: () => {
+        marquee.play();
+      },
     },
-  }, "<")
+    "<",
+  )
 
   // 介紹文字開跑
-  .from(".introduce-text",{
-    onStart:()=>{
-      introduce.play();
+  .from(
+    ".introduce-text",
+    {
+      onStart: () => {
+        introduce.play();
+      },
+      onReverseComplete: () => {
+        introduce.reverse();
+      },
     },
-    onReverseComplete: () => {
-      introduce.reverse();
-    }
-  },"+=0.1")
+    "+=0.1",
+  )
   // 留白（不會馬上滑到下一頁）
   .to({}, { duration: 1 });
-  ;
+// story-page下半部
 
+// 針對每一個 story-bottom-section 獨立製作動畫
+gsap.utils.toArray(".story-bottom-section").forEach((section) => {
+  // 1. 限制範圍：只抓「這個 Section」裡面的東西
+  const text = section.querySelector(".story-text");
+  const topImgs = section.querySelectorAll(".float-top");
+  const bottomImgs = section.querySelectorAll(".float-bottom");
 
-  // story-page下半部
   const tl2 = gsap.timeline({
-    scrollTrigger:{
-      trigger:".story-bottom-section",
-      start:"top 50%",
-      toggleActions: "play none none reverse",
-      scrub:1
+    scrollTrigger: {
+      trigger: section,
+      start: "top 90%",
+      end: "bottom 20%",
+      
+      markers: true,
     },
-    
   });
-// 時間軸
+
+  // 浮動效果
+  const wave1 = gsap.to(topImgs, {
+    yPercent: 40,
+    duration: 2,
+    yoyo: true,
+    repeat: -1,
+    paused: true,
+    ease: "power1.inOut",
+  });
+  const wave2 = gsap.to(bottomImgs, {
+    yPercent: -40,
+    duration: 2,
+    yoyo: true,
+    repeat: -1,
+    paused: true,
+    ease: "power1.inOut",
+  });
+
   tl2
-  .from(".story-text",{
-  
-    stagger:1,
-    duration:1,
-    y: 150,
-    
-    ease:"power3.out"
+
+    .from(text, {
+      y: 200,
+      opacity: 0,
+      duration: 1,
+      ease: "power3.out",
+    })
+
+    .from(
+      topImgs,
+      {
+        y: -400,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 1,
+        ease: "power3.out",
+        delay: 0.2,
+        onComplete: () => wave1.play(),
+        onReverseComplete: () => {
+          wave1.pause();
+          wave1.progress(0);
+        },
+      },
+
+      "<",
+    )
+
+    .from(
+      bottomImgs,
+      {
+        y: 400,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 1,
+        ease: "power3.out",
+        delay: 0.2,
+        onComplete: () => wave2.play(),
+        onReverseComplete: () => {
+          wave2.pause();
+          wave2.progress(0);
+        },
+      },
+      "<",
+    );
+});
+
+const highLight = document.querySelectorAll(".high-light");
+highLight.forEach((element) => {
+  gsap.to(element, {
+    backgroundSize: "100% 100%",
+    duration: 0.3,
+    delay: 0.1,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: element,
+      start: "top 100%",
+      toggleActions: "play none none reverse",
+      markers: true,
+    },
   });
-
-  // high-light單獨拉出來做
-  const highLight = document.querySelectorAll(".high-light");
-  highLight.forEach((element)=>{
-    gsap.to(element,{
-      backgroundSize: "100% 100%",
-      duration: 0.3,
-      delay:0.1,               
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: element,
-        start: "top 95%",          
-        toggleActions: "play none none reverse",
-        markers: true
-      }
-    });
-  });
-
- 
-
+});
